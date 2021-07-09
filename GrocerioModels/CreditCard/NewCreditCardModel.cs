1f259a6eeef685a4bde34985a030e66e59b93190
nas-cam <nasveta.c@gmail.com>
2021-07-09 16:29:32 +0200

    new credit card validation

--- GrocerioModels/CreditCard/NewCreditCardModel.cs
+++ GrocerioModels/CreditCard/NewCreditCardModel.cs
@@ -7,15 +7,24 @@ namespace GrocerioModels.CreditCard
 {
     public class NewCreditCardModel
     {
-        //TODO
-        //Validate real credit card data
         [Required]
+        [MinLength(5)]
+        [MaxLength(15)]
         public string CardHolder { get; set; }
+        
         [Required]
+        [MinLength(16)]
+        [MaxLength(16)]
         public string CardNumber { get; set; }
+
         [Required]
+        [MinLength(3)]
+        [MaxLength(5)]
         public string Expiration { get; set; }
+
         [Required]
+        [MaxLength(3)]
+        [MinLength(3)]
         public string CVV { get; set; }
     }
 }
